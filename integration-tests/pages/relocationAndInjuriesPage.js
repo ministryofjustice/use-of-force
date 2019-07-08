64bb7adac84913f5ba16287540448fafd8034c17
Gurnank Cheema <gurnank.cheema@digital.justice.gov.uk>
2019-07-08 06:42:32 +0100

    DCS 56: User now able to complete relocation and injuries details
    Inputs for the first 5 questions on this page have been constructed.
    Data verifed as being persisted in DB.

--- integration-tests/pages/relocationAndInjuriesPage.js
+++ integration-tests/pages/relocationAndInjuriesPage.js
@@ -3,6 +3,15 @@ const evidencePage = require('./evidencePage')
 
 export default () =>
   page('Relocation and injuries', {
+    fillForm: () => {
+      cy.get('[name="prisonerRelocation"]').select('segregationUnit')
+      cy.get('[name="prisonerCompliancy"]').select('compliant')
+      cy.get('[name="healthcareInvolved"]').check('yes')
+      cy.get('[name="healthcarePractionerName"]').type('Dr Smith')
+      cy.get('[name="prisonerInjuries"]').check('yes')
+      cy.get('[name="f213CompletedBy"]').type('Dr Taylor')
+      cy.get('[name="prisonerHospitalisation"]').check('yes')
+    },
     save: () => {
       cy.get('[data-next]').click()
       return evidencePage()
