8577d70c600df668b02ac1797cdfb96081d966fc
andrewrlee <1517745+andrewrlee@users.noreply.github.com>
2019-07-10 09:47:02 +0100

    DCS-37 Fixing bugs and improving tests
    Fixed a couple of bugs:
     * When a sole witness or staff member were persisted - when we re-visited the page
       the remove button was present. If that remove button was pressed we couldn't add
       another entry
     * When no witnesses or staff members were persisted, we would get a null pointer
       when trying to persist the form again
    
    Also test to ensure that we can render pre-saved data

--- integration-tests/pages/newIncidentPage.js
+++ integration-tests/pages/newIncidentPage.js
@@ -1,15 +1,37 @@
-const detailsPage = require('./detailsPage')
 const page = require('./page')
+const detailsPage = require('./detailsPage')
 
 export default () =>
   page('New use of force incident', {
     offenderName: () => cy.get('[data-offender-name]'),
     location: () => cy.get('#location'),
-    forceType: () => cy.get('[name="forceType"]'),
-    staffInvolved: index => cy.get(`#involved\\[${index}\\]\\[name\\]`),
+
+    forceType: {
+      check: value => cy.get('[name="forceType"]').check(value),
+      planned: () => cy.get("[name='forceType'][value='planned']"),
+      spontaneous: () => cy.get("[name='forceType'][value='spontaneous']"),
+    },
+
+    staffInvolved: index => ({
+      name: () => cy.get(`#involved\\[${index}\\]\\[name\\]`),
+      remove: () =>
+        cy
+          .get(`#involved\\[${index}\\]\\[name\\]`)
+          .parents('.add-another__item')
+          .find('button.add-another__remove-button'),
+    }),
     addAnotherStaff: () => cy.get('[data-qa-add-another-staff]'),
-    witnesses: index => cy.get(`#witnesses\\[${index}\\]\\[name\\]`),
+
+    witnesses: index => ({
+      name: () => cy.get(`#witnesses\\[${index}\\]\\[name\\]`),
+      remove: () =>
+        cy
+          .get(`#witnesses\\[${index}\\]\\[name\\]`)
+          .parents('.add-another__item')
+          .find('button.add-another__remove-button'),
+    }),
     addAnotherWitness: () => cy.get('[data-qa-add-another-witness]'),
+
     save: () => {
       cy.get('[data-next]').click()
       return detailsPage()
