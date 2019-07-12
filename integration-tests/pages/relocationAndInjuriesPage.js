01cef93b175f89c44502ac98d8618c878bbb6cea
Gurnank Cheema <gurnank.cheema@digital.justice.gov.uk>
2019-07-12 14:28:04 +0100

    DCS-56 new cypress test

--- integration-tests/pages/relocationAndInjuriesPage.js
+++ integration-tests/pages/relocationAndInjuriesPage.js
@@ -5,12 +5,15 @@ export default () =>
   page('Relocation and injuries', {
     fillForm: () => {
       cy.get('[name="prisonerRelocation"]').select('segregationUnit')
-      cy.get('[name="prisonerCompliancy"]').select('compliant')
+      cy.get('[name="relocationCompliancy"]').select('compliant')
       cy.get('[name="healthcareInvolved"]').check('yes')
       cy.get('[name="healthcarePractionerName"]').type('Dr Smith')
       cy.get('[name="prisonerInjuries"]').check('yes')
       cy.get('[name="f213CompletedBy"]').type('Dr Taylor')
       cy.get('[name="prisonerHospitalisation"]').check('yes')
+      cy.get('[name="staffMedicalAttention"]').check('yes')
+      cy.get('[name="staffMemberNeedingMedicalAttention[0][name]"]').type('Dan Smith')
+      cy.get('[name="staffMemberWentToHospital[0][name]"]').check('no')
     },
     save: () => {
       cy.get('[data-next]').click()
