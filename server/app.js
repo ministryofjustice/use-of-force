f886b02515b67c649ca82431cd81d7e3c2b93b99
andrewrlee <1517745+andrewrlee@users.noreply.github.com>
2019-07-26 15:29:13 +0100

    DCS-64 refactor of form service

--- server/app.js
+++ server/app.js
@@ -170,26 +170,13 @@ module.exports = function createApp({ signInService, incidentService, offenderSe
     app.use(csurf())
   }
 
-  // JWT token refresh
+  // token refresh
   app.use(async (req, res, next) => {
-    if (req.user && req.originalUrl !== '/logout') {
+    if (req.user) {
       const timeToRefresh = new Date() > req.user.refreshTime
       if (timeToRefresh) {
-        try {
-          const newToken = await signInService.getRefreshedToken(req.user)
-          req.user.token = newToken.token
-          req.user.refreshToken = newToken.refreshToken
-          logger.info(`existing refreshTime in the past by ${new Date() - req.user.refreshTime}`)
-          logger.info(
-            `updating time by ${newToken.refreshTime - req.user.refreshTime} from ${req.user.refreshTime} to ${
-              newToken.refreshTime
-            }`
-          )
-          req.user.refreshTime = newToken.refreshTime
-        } catch (error) {
-          logger.error(`Token refresh error: ${req.user.username}`, error.stack)
-          return res.redirect('/logout')
-        }
+        req.session.returnTo = req.originalUrl
+        return res.redirect('/login')
       }
     }
     return next()
