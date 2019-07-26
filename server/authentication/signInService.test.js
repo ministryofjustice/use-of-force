688700ab67a3a5d5a3694a733bca0ee3c3de70be
andrewrlee <1517745+andrewrlee@users.noreply.github.com>
2019-07-26 14:17:29 +0100

    DCS-60 Fixing test

--- server/authentication/signInService.test.js
+++ server/authentication/signInService.test.js
@@ -45,14 +45,14 @@ describe('signInService', () => {
 
     test('successfully get token', async () => {
       const expectedBody = 'grant_type=refresh_token&refresh_token=REFRESH_TOKEN-1'
-
+      const in15Mins = new Date('May 31, 2018 12:15:00').getTime()
       fakeOauthApi
         .post('/oauth/token', expectedBody)
         .reply(200, { access_token: 'NEW_ACCESS_TOKEN-1', refresh_token: 'REFRESH_TOKEN-2', expires_in: 300 })
 
       const output = await service.getRefreshedToken({ username: 'Bob', refreshToken: 'REFRESH_TOKEN-1' })
       expect(output).toEqual({
-        refreshTime: 1527765300000,
+        refreshTime: in15Mins,
         refreshToken: 'REFRESH_TOKEN-2',
         token: 'NEW_ACCESS_TOKEN-1',
       })
