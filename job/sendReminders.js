ce7b650e9dbb2771f4e492062228276dca8c252c
Andrew Lee <1517745+andrewrlee@users.noreply.github.com>
2019-08-16 09:43:22 +0100

    DCS-92 Run job every five minutes. Add environmental variables, prevent concurrent runs and increase history limit of failed and successful jobs (#97)

--- job/sendReminders.js
+++ job/sendReminders.js
@@ -2,3 +2,5 @@ const moment = require('moment')
 const logger = require('../log')
 
 logger.info(`send reminders job fired at: ${moment()}`)
+
+new Promise(done => setTimeout(done, 60 * 1000)).then(() => logger.info(`send reminders job ended at: ${moment()}`))
