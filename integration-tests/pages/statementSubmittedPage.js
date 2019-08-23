aa607cd355e570540b6f7d8f2e847ffc5ac8aa21
andrewrlee <1517745+andrewrlee@users.noreply.github.com>
2019-08-23 15:09:14 +0100

    DCS-98 fixing submitted page title

--- integration-tests/pages/statementSubmittedPage.js
+++ integration-tests/pages/statementSubmittedPage.js
@@ -2,7 +2,7 @@ const page = require('./page')
 const IncidentsPage = require('./incidentsPage')
 
 const submitStatementPage = () =>
-  page('Statement submitted', {
+  page('Your statement has been submitted', {
     finish: () => {
       cy.get('[data-qa=finish]').click()
       return IncidentsPage.verifyOnPage()
