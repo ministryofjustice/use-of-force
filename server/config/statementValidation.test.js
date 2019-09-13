3a171b8b2f6d206ec06816d67c741318f76b107c
bell-pa <peter.bell@digital.justice.gov.uk>
2019-09-13 21:00:26 +0100

    DCS-123 'write your statement' accepts 123456 as a valid year but blows up (UoF). restrict input values to smallInt. Add year check to requiredMonthIndex, becomes requiredMonthIndexNotInFuture

--- server/config/statementValidation.test.js
+++ server/config/statementValidation.test.js
@@ -17,193 +17,211 @@ const validInput = {
   statement: 'A statement about the incident',
 }
 
-it('successful input', () => {
-  const input = validInput
-  const { errors, formResponse } = check(input)
-
-  expect(errors).toEqual([])
-
-  expect(formResponse).toEqual({
-    lastTrainingMonth: 11,
-    lastTrainingYear: 1999,
-    jobStartYear: 1995,
-    statement: 'A statement about the incident',
-  })
-})
-
-it('no values supplied', () => {
-  const input = { submit: 'save-and-continue' }
-  const { errors, formResponse } = check(input)
-
-  expect(errors).toEqual([
-    {
-      href: '#lastTrainingMonth',
-      text: 'Select the month you last attended refresher training',
-    },
-    {
-      href: '#lastTrainingYear',
-      text: 'Enter the year you last attended refresher training',
-    },
-    {
-      href: '#jobStartYear',
-      text: 'Enter the year you joined the prison service',
-    },
-    {
-      href: '#statement',
-      text: 'Enter your statement',
-    },
-  ])
-
-  expect(formResponse).toEqual({})
-})
-
-describe('lastTrainingMonth', () => {
-  it('wrong type', () => {
-    const input = { ...validInput, lastTrainingMonth: 'asasas' }
-    const { errors, formResponse } = check(input)
-
-    expect(errors).toEqual([
-      {
-        href: '#lastTrainingMonth',
-        text: 'Select the month you last attended refresher training',
-      },
-    ])
-
-    expect(formResponse).toEqual({
-      lastTrainingYear: 1999,
-      jobStartYear: 1995,
-      statement: 'A statement about the incident',
-    })
-  })
-
-  it('not a valid month', () => {
-    const input = { ...validInput, lastTrainingMonth: '12' }
-    const { errors, formResponse } = check(input)
-
-    expect(errors).toEqual([
-      {
-        href: '#lastTrainingMonth',
-        text: 'Select the month you last attended refresher training',
-      },
-    ])
-
-    expect(formResponse).toEqual({
-      lastTrainingYear: 1999,
-      lastTrainingMonth: 12,
-      jobStartYear: 1995,
-      statement: 'A statement about the incident',
-    })
-  })
-})
-
-describe('lastTrainingYear', () => {
-  it('wrong type', () => {
-    const input = { ...validInput, lastTrainingYear: 'asasas' }
-    const { errors, formResponse } = check(input)
-
-    expect(errors).toEqual([
-      {
-        href: '#lastTrainingYear',
-        text: 'Enter the year you last attended refresher training',
-      },
-    ])
-
-    expect(formResponse).toEqual({
-      jobStartYear: 1995,
-      lastTrainingMonth: 11,
-      statement: 'A statement about the incident',
-    })
-  })
-
-  it('not a valid year', () => {
-    const input = { ...validInput, lastTrainingYear: `${moment().year() + 1}` }
-    const { errors, formResponse } = check(input)
-
-    expect(errors).toEqual([
-      {
-        href: '#lastTrainingYear',
-        text: 'Enter the year you last attended refresher training which is not in the future',
-      },
-    ])
-
-    expect(formResponse).toEqual({
-      lastTrainingYear: 2020,
-      lastTrainingMonth: 11,
-      jobStartYear: 1995,
-      statement: 'A statement about the incident',
-    })
-  })
-})
-
-describe('jobStartYear', () => {
-  it('wrong type', () => {
-    const input = { ...validInput, jobStartYear: 'asasas' }
-    const { errors, formResponse } = check(input)
-
-    expect(errors).toEqual([
-      {
-        href: '#jobStartYear',
-        text: 'Enter the year you joined the prison service',
-      },
-    ])
-
-    expect(formResponse).toEqual({
-      lastTrainingYear: 1999,
-      lastTrainingMonth: 11,
-      statement: 'A statement about the incident',
-    })
-  })
-
-  it('not a valid year', () => {
-    const input = { ...validInput, jobStartYear: `${moment().year() + 1}` }
-    const { errors, formResponse } = check(input)
-
-    expect(errors).toEqual([
-      {
-        href: '#jobStartYear',
-        text: 'Enter the year you joined the prison service which is not in the future',
-      },
-    ])
-
-    expect(formResponse).toEqual({
-      lastTrainingYear: 1999,
-      lastTrainingMonth: 11,
-      jobStartYear: 2020,
-      statement: 'A statement about the incident',
-    })
-  })
-})
-
-describe('statement', () => {
-  it('not empty space', () => {
-    const input = { ...validInput, statement: '   \n   \t   ' }
-    const { errors, formResponse } = check(input)
-
-    expect(errors).toEqual([
-      {
-        href: '#statement',
-        text: 'Enter your statement',
-      },
-    ])
-
-    expect(formResponse).toEqual({
-      lastTrainingYear: 1999,
-      lastTrainingMonth: 11,
-      jobStartYear: 1995,
-    })
-  })
-
-  it('surrounding space is trimmed', () => {
-    const input = { ...validInput, statement: '   \n A description  \t   ' }
-    const { errors, formResponse } = check(input)
+describe('statement validation', () => {
+  it('successful input', () => {
+    const { errors, formResponse } = check(validInput)
 
     expect(errors).toEqual([])
 
     expect(formResponse).toEqual({
-      lastTrainingYear: 1999,
       lastTrainingMonth: 11,
+      lastTrainingYear: 1999,
       jobStartYear: 1995,
-      statement: 'A description',
+      statement: 'A statement about the incident',
+    })
+  })
+
+  it('no values supplied', () => {
+    const input = { submit: 'save-and-continue' }
+    const { errors, formResponse } = check(input)
+
+    expect(errors).toEqual(
+      expect.arrayContaining([
+        {
+          href: '#lastTrainingMonth',
+          text: 'Select the month you last attended refresher training',
+        },
+        {
+          href: '#lastTrainingYear',
+          text: 'Enter the year you last attended refresher training',
+        },
+        {
+          href: '#jobStartYear',
+          text: 'Enter the year you joined the prison service',
+        },
+        {
+          href: '#statement',
+          text: 'Enter your statement',
+        },
+      ])
+    )
+
+    expect(formResponse).toEqual({
+      jobStartYear: NaN,
+      lastTrainingMonth: NaN,
+      lastTrainingYear: NaN,
+    })
+  })
+
+  describe('lastTrainingMonth', () => {
+    it('wrong type', () => {
+      const input = { ...validInput, lastTrainingMonth: 'asasas' }
+      const { errors, formResponse } = check(input)
+
+      expect(errors).toEqual([
+        {
+          href: '#lastTrainingMonth',
+          text: 'Select the month you last attended refresher training',
+        },
+      ])
+
+      expect(formResponse).toEqual({
+        lastTrainingYear: 1999,
+        lastTrainingMonth: NaN,
+        jobStartYear: 1995,
+        statement: 'A statement about the incident',
+      })
+    })
+
+    it('not a valid month', () => {
+      const input = { ...validInput, lastTrainingMonth: '12' }
+      const { errors, formResponse } = check(input)
+
+      expect(errors).toEqual([
+        {
+          href: '#lastTrainingMonth',
+          text: 'Select the month you last attended refresher training',
+        },
+      ])
+
+      expect(formResponse).toEqual({
+        lastTrainingYear: 1999,
+        lastTrainingMonth: 12,
+        jobStartYear: 1995,
+        statement: 'A statement about the incident',
+      })
+    })
+  })
+
+  describe('lastTrainingYear', () => {
+    it('wrong type', () => {
+      const input = { ...validInput, lastTrainingYear: 'asasas' }
+      const { errors, formResponse } = check(input)
+
+      expect(errors).toEqual([
+        {
+          href: '#lastTrainingYear',
+          text: 'Enter the year you last attended refresher training',
+        },
+        {
+          href: '#lastTrainingMonth',
+          text: 'Select the month you last attended refresher training',
+        },
+      ])
+
+      expect(formResponse).toEqual({
+        jobStartYear: 1995,
+        lastTrainingMonth: 11,
+        lastTrainingYear: NaN,
+        statement: 'A statement about the incident',
+      })
+    })
+
+    it('not a valid year', () => {
+      const input = { ...validInput, lastTrainingYear: `${moment().year() + 1}` }
+      const { errors, formResponse } = check(input)
+
+      expect(errors).toEqual([
+        {
+          href: '#lastTrainingYear',
+          text: 'Enter the year you last attended refresher training which is not in the future',
+        },
+        {
+          href: '#lastTrainingMonth',
+          text: 'Select the month you last attended refresher training',
+        },
+      ])
+
+      expect(formResponse).toEqual({
+        lastTrainingYear: 2020,
+        lastTrainingMonth: 11,
+        jobStartYear: 1995,
+        statement: 'A statement about the incident',
+      })
+    })
+  })
+
+  describe('jobStartYear', () => {
+    it('wrong type', () => {
+      const input = { ...validInput, jobStartYear: 'asasas' }
+      const { errors, formResponse } = check(input)
+
+      expect(errors).toEqual([
+        {
+          href: '#jobStartYear',
+          text: 'Enter the year you joined the prison service',
+        },
+      ])
+
+      expect(formResponse).toEqual({
+        jobStartYear: NaN,
+        lastTrainingYear: 1999,
+        lastTrainingMonth: 11,
+        statement: 'A statement about the incident',
+      })
+    })
+
+    it('not a valid year', () => {
+      const input = { ...validInput, jobStartYear: `${moment().year() + 1}` }
+      const { errors, formResponse } = check(input)
+
+      expect(errors).toEqual([
+        {
+          href: '#jobStartYear',
+          text: 'Enter the year you joined the prison service which is not in the future',
+        },
+      ])
+
+      expect(formResponse).toEqual({
+        lastTrainingYear: 1999,
+        lastTrainingMonth: 11,
+        jobStartYear: 2020,
+        statement: 'A statement about the incident',
+      })
+    })
+  })
+
+  describe('statement', () => {
+    it('not empty space', () => {
+      const input = { ...validInput, statement: '   \n   \t   ' }
+      const { errors, formResponse } = check(input)
+
+      expect(errors).toEqual([
+        {
+          href: '#statement',
+          text: 'Enter your statement',
+        },
+      ])
+
+      expect(formResponse).toEqual({
+        lastTrainingYear: 1999,
+        lastTrainingMonth: 11,
+        jobStartYear: 1995,
+      })
+    })
+
+    it('surrounding space is trimmed', () => {
+      const input = { ...validInput, statement: '   \n A description  \t   ' }
+      const { errors, formResponse } = check(input)
+
+      expect(errors).toEqual([])
+
+      expect(formResponse).toEqual({
+        lastTrainingYear: 1999,
+        lastTrainingMonth: 11,
+        jobStartYear: 1995,
+        statement: 'A description',
+      })
     })
   })
 })
--- server/utils/fieldValidation.test.js
+++ server/utils/fieldValidation.test.js
@@ -1,15 +1,65 @@
-const { isValid } = require('./fieldValidation')
+const moment = require('moment')
+const { isValid, validate } = require('./fieldValidation')
 
-describe('isValid', () => {
-  test('Check valid ', () => {
-    expect(isValid('optionalInvolvedStaff', [{ username: 'Bob' }])).toEqual(true)
-    expect(isValid('optionalInvolvedStaff', [])).toEqual(true)
+describe('fieldValidation', () => {
+  describe('isValid', () => {
+    test('Check valid ', () => {
+      expect(isValid('optionalInvolvedStaff', [{ username: 'Bob' }])).toEqual(true)
+      expect(isValid('optionalInvolvedStaff', [])).toEqual(true)
+    })
+
+    test('invalid', () => {
+      expect(isValid('optionalInvolvedStaff', [{ username: 'Bob', age: 29 }])).toEqual(false)
+      expect(isValid('optionalInvolvedStaff', true)).toEqual(false)
+      expect(isValid('optionalInvolvedStaff', [{ username: '' }])).toEqual(false)
+      expect(isValid('optionalInvolvedStaff', [{ bob: 'Bob' }])).toEqual(false)
+    })
   })
 
-  test('invalid', () => {
-    expect(isValid('optionalInvolvedStaff', [{ username: 'Bob', age: 29 }])).toEqual(false)
-    expect(isValid('optionalInvolvedStaff', true)).toEqual(false)
-    expect(isValid('optionalInvolvedStaff', [{ username: '' }])).toEqual(false)
-    expect(isValid('optionalInvolvedStaff', [{ bob: 'Bob' }])).toEqual(false)
+  describe('validate', () => {
+    const fields = [
+      {
+        month: {
+          responseType: 'requiredMonthIndexNotInFuture_year',
+          validationMessage: 'Select the month',
+        },
+      },
+      {
+        year: {
+          responseType: 'requiredYearNotInFuture',
+        },
+      },
+    ]
+
+    const selectMonth = {
+      href: '#month',
+      text: 'Select the month',
+    }
+
+    const yearLessEq2019 = {
+      href: '#year',
+      text: '"year" must be less than or equal to 2019',
+    }
+
+    describe('requiredMonthIndexNotInFuture', () => {
+      const thisMonth = moment().month()
+      const nextMonth = moment().month() + 1
+
+      test('This month of this year valid', () => {
+        expect(validate(fields, { month: thisMonth, year: '2019' })).toEqual([])
+      })
+
+      test('Next month of this year invalid', () => {
+        expect(validate(fields, { month: nextMonth, year: '2019' })).toEqual([selectMonth])
+      })
+
+      test('Next year invalid', () => {
+        expect(validate(fields, { month: '01', year: '2020' })).toEqual([yearLessEq2019])
+      })
+
+      test('Last month of previous year valid', () => {
+        expect(validate(fields, { month: '11', year: '2018' })).toEqual([])
+      })
+    })
   })
 })
