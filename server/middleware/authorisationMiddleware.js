81efcc733533ff0f2f30704202109af0775ddef8
andrewrlee <1517745+andrewrlee@users.noreply.github.com>
2019-09-14 06:55:00 +0100

    DCS-129 Coping with no authorities and improving tests

--- server/middleware/authorisationMiddleware.js
+++ server/middleware/authorisationMiddleware.js
@@ -2,7 +2,7 @@ const jwtDecode = require('jwt-decode')
 
 module.exports = (req, res, next) => {
   if (res.locals && res.locals.user && res.locals.user.token) {
-    const roles = jwtDecode(res.locals.user.token).authorities
+    const { authorities: roles = [] } = jwtDecode(res.locals.user.token)
     res.locals.user = { isReviewer: roles.includes('ROLE_USE_OF_FORCE_REVIEWER'), ...res.locals.user }
     return next()
   }
