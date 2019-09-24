4ed4c128e171edc1755143f1931fee694cb1399c
andrewrlee <1517745+andrewrlee@users.noreply.github.com>
2019-09-24 07:46:24 +0100

    DCS-144 Adding application insights to reminder job
    Also increasing number of reminders to process to a maximum of 50 every 5 minutes (up from 1)

--- server/utils/azure-appinsights.js
+++ server/utils/azure-appinsights.js
@@ -1,16 +1,24 @@
 const appInsights = require('applicationinsights')
 const applicationVersion = require('../application-version')
 
-const { packageData } = applicationVersion
-if (process.env.APPINSIGHTS_INSTRUMENTATIONKEY) {
-  // eslint-disable-next-line no-console
-  console.log('Enabling azure application insights')
-  appInsights
-    .setup()
-    .setDistributedTracingMode(appInsights.DistributedTracingModes.AI_AND_W3C)
-    .start()
-  module.exports = appInsights.defaultClient
-  appInsights.defaultClient.context.tags['ai.cloud.role'] = `${packageData.name}`
-} else {
-  module.exports = null
+const defaultName = () => {
+  const {
+    packageData: { name },
+  } = applicationVersion
+  return name
+}
+
+module.exports = (name = defaultName()) => {
+  if (process.env.APPINSIGHTS_INSTRUMENTATIONKEY) {
+    // eslint-disable-next-line no-console
+    console.log('Enabling azure application insights')
+    appInsights
+      .setup()
+      .setDistributedTracingMode(appInsights.DistributedTracingModes.AI_AND_W3C)
+      .start()
+    const client = appInsights.defaultClient
+    client.context.tags['ai.cloud.role'] = `${name}`
+    return client
+  }
+  return null
 }
