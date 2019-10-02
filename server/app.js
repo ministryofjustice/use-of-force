1a3bceda0005abf577ecbd108a0bd0183f960644
Andrew Lee <1517745+andrewrlee@users.noreply.github.com>
2019-10-02 08:18:43 +0100

    DCS-157 Fixing error logging (#208)
    Error handlers require magical fourth parameter otherwise they don't actually register!

--- server/app.js
+++ server/app.js
@@ -271,7 +271,7 @@ module.exports = function createApp({
 }
 
 // eslint-disable-next-line no-unused-vars
-function renderErrors(error, req, res) {
+function renderErrors(error, req, res, next) {
   logger.error(error)
 
   // code to handle unknown errors
