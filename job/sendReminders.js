0ec2d4dac69de07fbb6ca5bef42020e801ca564e
andrewrlee <1517745+andrewrlee@users.noreply.github.com>
2020-07-07 15:08:23 +0100

    DCS-556 fixing job

--- job/sendReminders.js
+++ job/sendReminders.js
@@ -10,7 +10,7 @@ const logger = require('../log')
 const { notificationServiceFactory } = require('../server/services/notificationService')
 const EmailResolver = require('./reminders/emailResolver')
 
-const incidentClient = require('../server/data/incidentClient')
+const { IncidentClient } = require('../server/data/incidentClient')
 const statementsClient = require('../server/data/statementsClient')
 
 const { authClientBuilder, systemToken } = require('../server/data/authClientBuilder')
@@ -25,7 +25,7 @@ const notificationService = notificationServiceFactory(eventPublisher)
 
 const reminderSender = reminderSenderFactory(notificationService)
 
-const poll = reminderPoller(db, incidentClient, reminderSender, emailResolver)
+const poll = reminderPoller(db, new IncidentClient(), reminderSender, emailResolver)
 
 async function runJob() {
   eventPublisher.publish({ name: 'StartingToSendReminders', properties: {}, detail: null })
