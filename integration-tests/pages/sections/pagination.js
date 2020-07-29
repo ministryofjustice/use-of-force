5701fcd1c64fe84111c5b89f1f1a6b5dab416feb
Andrew Lee <1517745+andrewrlee@users.noreply.github.com>
2020-07-29 15:39:06 +0100

    DCS-571 Adding pagination to bottom of the page too (#379)
    * DCS-571 Adding pagination to bottom of the page too
    
    * DCS-571 fix test
    
    Co-authored-by: GCheema <50441412+GurnankCheema@users.noreply.github.com>

--- integration-tests/pages/sections/pagination.js
+++ integration-tests/pages/sections/pagination.js
@@ -1,13 +1,17 @@
+const firstPagination = () => cy.get('.moj-pagination').first()
+
 module.exports = {
   pagination: () => cy.get('.moj-pagination'),
   pageLinks: () =>
-    cy.get(`.moj-pagination__item`).spread((...rest) =>
-      rest.map(element => ({
-        href: Cypress.$(element).find('a').attr('href'),
-        text: Cypress.$(element).text().trim(),
-        selected: Cypress.$(element).find('a').attr('href') === undefined,
-      }))
-    ),
-  pageResults: () => cy.get('.moj-pagination__results'),
-  clickLinkWithText: text => cy.get(`.moj-pagination__item`).contains(text).click(),
+    firstPagination()
+      .find(`.moj-pagination__item`)
+      .spread((...rest) =>
+        rest.map(element => ({
+          href: Cypress.$(element).find('a').attr('href'),
+          text: Cypress.$(element).text().trim(),
+          selected: Cypress.$(element).find('a').attr('href') === undefined,
+        }))
+      ),
+  pageResults: () => firstPagination().find('.moj-pagination__results'),
+  clickLinkWithText: text => firstPagination().get(`.moj-pagination__item`).contains(text).click(),
 }
