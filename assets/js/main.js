b93b5644d2660e788e12ae5718335f3839851461
G <gurnank@outlook.com>
2020-08-26 17:14:08 +0100

    DCS-583: removed duplicate identifiers

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,22 @@
-$(document).ready(function() {
+$(document).ready(function () {
   $('.js-submitLink').click(e => {
     e.preventDefault()
     var form = $('form')
     var value = $(e.target).attr('data-value')
 
-    var input = $('<input>')
-      .attr('type', 'hidden')
-      .attr('name', 'submitType')
-      .val(value)
+    var input = $('<input>').attr('type', 'hidden').attr('name', 'submitType').val(value)
     form.append(input)
     form.submit()
   })
 
-  $('.date-input').each(function(index, element) {
+  $('.date-input').each(function (index, element) {
     const disableFutureDates = Boolean($(element).attr('disable-future-dates'))
     const disablePastDates = Boolean($(element).attr('disable-past-dates'))
     const maxDate = disableFutureDates ? '0' : undefined
     const minDate = disablePastDates ? '0' : undefined
     const dateFormat = $(element).attr('date-format') || 'd M yy'
 
-      $(element).datepicker({
+    $(element).datepicker({
       dateFormat,
       showOtherMonths: true,
       selectOtherMonths: true,
