9529e5b74964433ff6b6516c3aa219267b85c55f
Louise Norris <louise.norris@digital.justice.gov.uk>
2021-04-01 11:57:13 +0100

    DCS-905 added request removal post to controller

--- integration-tests/integration/statements/request-removal.spec.js
+++ integration-tests/integration/statements/request-removal.spec.js
@@ -1,5 +1,6 @@
 const moment = require('moment')
 const RequestRemovalPage = require('../../pages/yourStatements/requestRemovalPage')
+const RemovalRequestedPage = require('../../pages/yourStatements/removalRequestedPage')
 const { ReportStatus } = require('../../../dist/server/config/types')
 
 context('Request removal', () => {
@@ -29,6 +30,23 @@ context('Request removal', () => {
       requestRemovalPage.prisonName().contains('Moorland')
       requestRemovalPage.reason().type('This is my reason to be removed')
       requestRemovalPage.requestToBeRemoved().click()
+
+      RemovalRequestedPage.verifyOnPage()
+    })
+  })
+
+  it('A user will be shown a validation message when no reason is provided', () => {
+    seedReport().then(statementId => {
+      const requestRemovalPage = RequestRemovalPage.visit(statementId)
+      requestRemovalPage.incidentDate().contains('22 January 2019')
+      requestRemovalPage.incidentTime().contains('09:57')
+      requestRemovalPage.prisonName().contains('Moorland')
+      requestRemovalPage.reason().should('be.empty')
+      requestRemovalPage.requestToBeRemoved().click()
+
+      requestRemovalPage.errorSummaryTitle().contains('There is a problem')
+      requestRemovalPage.errorSummaryBody().contains('Enter why you should be removed from this incident')
+      requestRemovalPage.inlineError().contains('Enter why you should be removed from this incident')
     })
   })
 })
--- server/routes/unauthenticated/requestRemoval.ts
+++ server/routes/unauthenticated/requestRemoval.ts
@@ -1,11 +1,16 @@
 import type { Request, RequestHandler } from 'express'
-import type { ReportService } from '../../services'
+import { paths } from '../../config/incident'
+import type { ReportService, StatementService } from '../../services'
 import { SystemToken } from '../../types/uof'
 
 const extractStatementId = (req: Request): number => parseInt(req.params.statementId, 10)
 
 export default class RemovalRequest {
-  constructor(private readonly reportService: ReportService, private readonly systemToken: SystemToken) {}
+  constructor(
+    private readonly reportService: ReportService,
+    private readonly statementService: StatementService,
+    private readonly systemToken: SystemToken
+  ) {}
 
   view: RequestHandler = async (req, res) => {
     const statementId = extractStatementId(req)
@@ -19,7 +24,22 @@ export default class RemovalRequest {
 
   submit: RequestHandler = async (req, res) => {
     const statementId = extractStatementId(req)
+    const { reason } = req.body
+    if (!reason) {
+      req.flash('errors', [
+        {
+          text: 'Enter why you should be removed from this incident',
+          href: '#reason',
+        },
+      ])
+      return res.redirect(paths.requestRemoval(statementId))
+    }
 
-    return res.redirect(`/request-removal/${statementId}`)
+    await this.statementService.requestStatementRemoval(statementId, reason)
+    return res.redirect('/removal-requested')
+  }
+
+  viewConfirmation: RequestHandler = (req, res) => {
+    return res.render(`pages/statement/removal-requested.html`)
   }
 }
