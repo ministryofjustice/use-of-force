fcba255c92b87ee5fff5f3a2703185c317241004
Michal Nawrocki <michal.nawrocki@digital.justice.gov.uk>
2024-11-21 09:04:01 +0000

    MAP-1839 adding view to simulate flyway db for generic-service db refresh requirments

--- migrations/20241120000001_form_db.js
+++ migrations/20241120000001_form_db.js
@@ -1,3 +1,3 @@
 exports.up = async knex => {
-  await knex.raw(`CREATE VIEW flyway_schema_history AS SELECT name as view FROM knex_migrations ORDER BY id;`)
+  await knex.raw(`CREATE VIEW flyway_schema_history AS SELECT name as version FROM knex_migrations ORDER BY id;`)
 }
