a9fdf4b03de70fcd4a9c4a44a66fb6041050b150
Gurnank <gurnank.cheema@digital.justice.gov.uk>
2025-04-08 09:18:24 +0100

    MAP-2010 update unit tests. Also, try-catch removed from locationClient as already covered by RestClient

--- server/config.ts
+++ server/config.ts
@@ -13,6 +13,15 @@ function get(name, fallback, options = {}) {
 
 const requiredInProduction = { requireInProduction: true }
 
+export class AgentConfig {
+  // Sets the working socket to timeout after timeout milliseconds of inactivity on the working socket.
+  timeout: number
+
+  constructor(timeout = 8000) {
+    this.timeout = timeout
+  }
+}
+
 export default {
   staticResourceCacheDuration: '1h',
   db: {
--- server/data/locationClient.ts
+++ server/data/locationClient.ts
@@ -12,21 +12,15 @@ export default class LocationClient {
       logger.info(`locationId ${incidentLocationId} has invalid length for UUID`)
       return undefined
     }
-    try {
       logger.info(`Location Client getting details for location: ${incidentLocationId}`)
-      const result = await this.restClient.get({ path: `/locations/${incidentLocationId}?formatLocalName=true` })
-      return result as LocationInPrison
-    } catch (error) {
-      if (error?.status !== 404) throw error
-    }
-    return undefined
+      return this.restClient.get({ path: `/locations/${incidentLocationId}?formatLocalName=true` })
   }
 
   async getLocations(
     prisonId: string,
     usageType: NonResidentialUsageType = NonResidentialUsageType.OCCURRENCE
   ): Promise<LocationInPrison[]> {
-    logger.info(`getting locations for prison ${prisonId} and usageType ${usageType}`)
+    logger.info(`Location Client getting locations for prison ${prisonId} and usageType ${usageType}`)
     return this.restClient.get({
       path: `/locations/prison/${prisonId}/non-residential-usage-type/${usageType}?formatLocalName=true&sortByLocalName=true`,
     })
