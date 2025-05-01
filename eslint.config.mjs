16a464939fd0c08487ca493f77ff443c043597af
Gurnank <gurnank.cheema@digital.justice.gov.uk>
2025-05-01 10:26:22 +0100

    MAP-2388 remove space

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,4 +3,3 @@ import hmppsConfig from '@ministryofjustice/eslint-config-hmpps'
 export default hmppsConfig({
     extraIgnorePaths: ['assets', 'public', 'cypress.config.ts', 'reporter-config.json'],
     })
-      
