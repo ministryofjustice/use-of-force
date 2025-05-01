7eb701e2fd611d81dd6676b56e433a62fb25b13a
Gurnank <gurnank.cheema@digital.justice.gov.uk>
2025-05-01 09:41:32 +0100

    MAP-2388 add new DB table to hold report edit history
    Also updated eslint to be inline with template project as could not commit without the change.

---
+++ eslint.config.mjs
@@ -0,0 +1,6 @@
+import hmppsConfig from '@ministryofjustice/eslint-config-hmpps'
+
+export default hmppsConfig({
+    extraIgnorePaths: ['assets', 'public', 'cypress.config.ts', 'reporter-config.json'],
+    })
+      
