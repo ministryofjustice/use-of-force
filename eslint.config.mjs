d6ea6f7d984f57454fc370edc3040d08f26fcdfe
Gurnank <gurnank.cheema@digital.justice.gov.uk>
2025-05-01 10:36:30 +0100

    MAP-2388 migration for new report edit history table plus update eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,5 @@
+import hmppsConfig from '@ministryofjustice/eslint-config-hmpps'
+
+export default hmppsConfig({
+    extraIgnorePaths: ['assets', 'public', 'cypress.config.ts', 'reporter-config.json'],
+    })
