9a6b2262db03c65395eca06b0db9658f06241015
Gurnank <gurnank.cheema@digital.justice.gov.uk>
2025-05-20 16:37:32 +0100

    MAP-2010 config eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,33 @@
 import hmppsConfig from '@ministryofjustice/eslint-config-hmpps'
 
-export default hmppsConfig({ extraIgnorePaths: ['integration-tests/', 'migrations/', 'assets/', '*.test.*'] })
+
+export default [
+  ...hmppsConfig({
+    extraPathsAllowingDevDependencies: ['**/__testutils/**'],
+    extraIgnorePaths: ['integration-tests/', 'migrations/', 'assets/', '*.test.*'] 
+  }),
+  {
+    rules: {
+      'no-param-reassign': 'off',
+      '@typescript-eslint/no-shadow': 'off',
+      'import/no-cycle': 'off',
+      'max-classes-per-file': 'off',
+    },
+  },
+  {
+    files: ['**/*.ts', '**/*.js'],
+    rules: {
+      'import/no-unresolved': 'off',
+      'import/extensions': 'off', 
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: 'res|next|^err|_',
+          ignoreRestSiblings: true,
+          caughtErrors: 'all',
+          caughtErrorsIgnorePattern: '^_',
+        },
+      ],
+    },
+  },
+]
