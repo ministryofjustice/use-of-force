30a8000d0e4886ed8285b8ae9cb07a0b809729ab
Barry Gee <barry.gee@justice.gov.uk>
2025-10-22 08:50:08 +0100

    MAP-2487 - Update validation error messages

--- server/config/forms/reasonForAddingStaffForm.js
+++ server/config/forms/reasonForAddingStaffForm.js
@@ -7,22 +7,22 @@ const reasonForAddingStaffSchema = joi.object({
     .valid('errorInReport', 'somethingMissingFromReport', 'anotherReasonForEdit')
     .required()
     .messages({
-      'any.required': 'Select a reason for adding this person',
-      'any.only': 'Select a valid reason for adding this person',
+      'any.required': 'Provide a reason for adding this person',
+      'any.only': 'Provide a reason for adding this person',
     }),
   reasonText: joi.when('reason', {
     is: 'anotherReasonForEdit',
     then: joi.string().trim().min(3).max(250).required().messages({
-      'any.required': 'Enter a reason',
-      'string.empty': 'Enter a reason',
+      'any.required': 'Specify the reason for adding this person',
+      'string.empty': 'Specify the reason for adding this person',
       'string.min': 'Reason must be at least 3 characters',
       'string.max': 'Reason must be 250 characters or fewer',
     }),
     otherwise: joi.string().allow('').optional(),
   }),
   reasonAdditionalInfo: joi.string().max(500).required().messages({
-    'any.required': 'Enter additional information',
-    'string.empty': 'Enter additional information',
+    'any.required': 'Provide additional information to explain why you are adding this person',
+    'string.empty': 'Provide additional information to explain why you are adding this person',
     'string.max': 'Additional information must be 500 characters or fewer',
   }),
 })
