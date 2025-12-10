2dddfb7e2c5ea79163f6947538e8c7ec4278166b
dafhoustonmoj <dafydd.houston@justice.gov.uk>
2025-12-10 09:13:34 +0000

    MAP-3076: replace deprecated node-sass with sass (MAP-3046)

--- .allowed-scripts.mjs
+++ .allowed-scripts.mjs
@@ -6,7 +6,7 @@ export default configureAllowedScripts({
     'node_modules/cypress@13.17.0': 'ALLOW',
     // Provides native integration, supporting the ability to write dtrace probes for bunyan
     'node_modules/dtrace-provider@0.8.8': 'ALLOW',
-    'node_modules/node-sass@9.0.0': 'ALLOW',
+    'node_modules/@parcel/watcher@2.5.1': 'ALLOW',
     // Needed by jest for running tests in watch mode
     'node_modules/fsevents@2.3.3': 'ALLOW',
     // Native solution to quickly resolve module paths, used by jest and eslint
