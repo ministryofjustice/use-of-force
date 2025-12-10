1f25030a6195f3b3e40f690f20684ebe4371c7e4
dafhoustonmoj <dafydd.houston@justice.gov.uk>
2025-12-10 14:19:24 +0000

    MAP-3076: remove hmpps eslint config. not added yet

--- eslint.config.mjs
+++
@@ -1,7 +0,0 @@
-import hmppsConfig from '@ministryofjustice/eslint-config-hmpps'
-
-const config = hmppsConfig({
-  extraPathsAllowingDevDependencies: ['.allowed-scripts.mjs'],
-})
-
-export default config
