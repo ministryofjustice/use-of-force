7f85917f816f193aa542d36c22f0d0a2ec5a7060
dafhoustonmoj <dafydd.houston@justice.gov.uk>
2026-03-16 16:26:14 +0000

    MAP-294B: fix logger

--- logger.ts
+++ logger.ts
@@ -4,6 +4,6 @@ import config from './server/config'
 
 const formatOut = bunyanFormat({ outputMode: 'short', color: !config.production })
 
-const logger = bunyan.createLogger({ name: 'HMPPS Typescript Template', stream: formatOut, level: 'debug' })
+const logger = bunyan.createLogger({ name: 'use of force', stream: formatOut, level: 'debug' })
 
 export default logger
